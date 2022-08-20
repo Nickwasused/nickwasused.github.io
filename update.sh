37d664d8ce622e355a70724076a7c6d67b34ae06
Nickwasused <nickwasused.services@protonmail.com>
2022-08-20 12:41:36 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 git remote update
 if git status | grep -q 'use "git pull" to update your local branch'; then
         echo "updating..."
+        git stash
         git pull
         deno task build
         systemctl restart nickwasused
