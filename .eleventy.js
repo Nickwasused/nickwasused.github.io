c2b6e6519775d0143cdd21625f6c0c89cfe5738c
nickwasused <github.lix1s@simplelogin.co>
2023-07-30 16:02:56 +0200

    change to scss and hashed css file name

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const pluginRev = require("eleventy-plugin-rev");
+const eleventySass = require("eleventy-sass");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/basic.css");
@@ -7,6 +9,11 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/robots.txt");
     eleventyConfig.addPassthroughCopy("src/{,!(_site)/**/}*.webp");
     eleventyConfig.addPlugin(pluginRss);
+    eleventyConfig.addPlugin(pluginRev);
+    eleventyConfig.addPlugin(eleventySass, {
+      rev: true
+    });
+  
    
     return {
         dir: {
