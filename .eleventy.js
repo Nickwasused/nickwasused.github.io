9656f20be0dc3931560d96078b4791008b22124a
nickwasused <github.lix1s@simplelogin.co>
2023-07-30 14:56:19 +0200

    add missing headers

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const pluginRss = require("@11ty/eleventy-plugin-rss");
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/basic.css");
     eleventyConfig.addPassthroughCopy("src/lozad.js");
+    eleventyConfig.addPassthroughCopy("src/_headers");
     eleventyConfig.addPassthroughCopy("src/{,!(_site)/**/}*.webp");
     eleventyConfig.addPlugin(pluginRss);
    
