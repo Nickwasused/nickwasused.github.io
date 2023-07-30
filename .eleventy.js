b99cc0502f7a9255df8098d103d7b34dc9c63373
nickwasused <github.lix1s@simplelogin.co>
2023-07-30 15:03:14 +0200

    add robots.txt

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/basic.css");
     eleventyConfig.addPassthroughCopy("src/lozad.js");
     eleventyConfig.addPassthroughCopy("src/_headers");
+    eleventyConfig.addPassthroughCopy("src/robots.txt");
     eleventyConfig.addPassthroughCopy("src/{,!(_site)/**/}*.webp");
     eleventyConfig.addPlugin(pluginRss);
    
