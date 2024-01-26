640f1d9c735bd448d7ee0e95c02d676eb1d73a17
Nickwasused <github.lix1s@simplelogin.co>
2024-01-26 14:51:00 +0100

    add cname

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/lozad.js");
     eleventyConfig.addPassthroughCopy("src/_headers");
     eleventyConfig.addPassthroughCopy("src/_redirects");
+    eleventyConfig.addPassthroughCopy("src/CNAME");
     eleventyConfig.addPassthroughCopy("src/robots.txt");
     eleventyConfig.addPassthroughCopy("src/{,!(_site)/**/}*.webp");
     eleventyConfig.addPlugin(pluginRss);
