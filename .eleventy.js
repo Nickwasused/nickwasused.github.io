daec3af473e768aba3fce8b0b672f7051dc3d6e0
Nickwasused <github.lix1s@simplelogin.co>
2024-05-05 14:13:33 +0200

    add feed styling

--- .eleventy.js
+++ .eleventy.js
@@ -11,6 +11,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/_redirects");
     eleventyConfig.addPassthroughCopy("src/CNAME");
     eleventyConfig.addPassthroughCopy("src/robots.txt");
+    eleventyConfig.addPassthroughCopy("src/simple.xslt");
     eleventyConfig.addPassthroughCopy("src/{,!(_site)/**/}*.webp");
     eleventyConfig.addPassthroughCopy("src/{,!(_site)/**/}*.avif");
     eleventyConfig.addPlugin(pluginRss);
