94fd56bf37a0a121022b217d038e73e085786142
Nickwasused <github.lix1s@simplelogin.co>
2025-07-20 10:44:05 +0200

    update to 11ty 3.1

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,19 +1,16 @@
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-const pluginRev = require("eleventy-plugin-rev");
-const eleventySass = require("eleventy-sass");
+import pluginRss from "@11ty/eleventy-plugin-rss";
 
-module.exports = function(eleventyConfig) {
+export default async function(eleventyConfig) {
     eleventyConfig.addFilter("custom_date", function(value) { return value.toLocaleDateString("de-DE", {
       year: "numeric",
       month: "2-digit",
       day: "2-digit",
     }) });
     eleventyConfig.addFilter("custom_date_iso", function(value) { return value.toISOString().split('T')[0] });
-    eleventyConfig.addPassthroughCopy("src/basic.css");
-    eleventyConfig.addPassthroughCopy("src/lozad.js");
-    eleventyConfig.addPassthroughCopy("src/_headers");
-    eleventyConfig.addPassthroughCopy("src/_redirects");
-    eleventyConfig.addPassthroughCopy("src/CNAME");
+    eleventyConfig.addPassthroughCopy({"src/basic.css": "basic.css"});
+    eleventyConfig.addPassthroughCopy({"src/_headers": "_headers"});
+    eleventyConfig.addPassthroughCopy({"src/_redirects": "_redirects"});
+    eleventyConfig.addPassthroughCopy({"src/CNAME": "CNAME"});
     eleventyConfig.addPassthroughCopy("src/robots.txt");
     eleventyConfig.addPassthroughCopy("src/simple.xslt");
     eleventyConfig.addPassthroughCopy("src/{,!(_site)/**/}*.webp");
@@ -21,12 +18,7 @@ module.exports = function(eleventyConfig) {
     // eleventyConfig.addPassthroughCopy("src/_posts/2024-06-14-discord-archiving/report.htm");
     eleventyConfig.addPassthroughCopy("src/_posts/2024-06-14-discord-archiving/archive.sh");
     eleventyConfig.addPlugin(pluginRss);
-    eleventyConfig.addPlugin(pluginRev);
-    eleventyConfig.addPlugin(eleventySass, {
-      rev: true
-    });
-  
-   
+
     return {
         dir: {
           input: "src", 
